447ef80742061a5f765cd2a7122457e8907df854
Vyshakh Jayan <vyshakh.jayan@thoughtworks.com>
2019-10-16 06:32:35 +0530

    Fixed gitignore

--- src/main/java/com/bootcamp/Application.java
+++ src/main/java/com/bootcamp/Application.java
@@ -4,6 +4,6 @@ public class Application {
     public static void main(String[] args) {
         Game game = new Game(new Player(new ConsoleBehaviour(new ScannerWrapper())), new Player(new CheatBehaviour()),1);
         Score score = game.start();
-        System.out.println("Player 1 score: " + score.getPlayer1Score() + ". Player 2 score: " + score.getPlayer2Score());
+        System.out.println(score);
     }
 }
